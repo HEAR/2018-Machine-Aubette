bb1d1f55e6a4be0107071d707b8c7a555412a8d0
Loïc <hello@loichorellou.net>
2018-11-16 09:41:27 +0100

    update project

---
+++ python/aubette.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import selenium.webdriver as webdriver
+import os
+import requests
+
+from threading import Timer
+
+# 	   https://www.instagram.com/explore/tags/nuitdelaubette/
+# 	   https://www.instagram.com/explore/tags/lanuitdelaubette/
+# 	   https://www.instagram.com/explore/tags/machineaubette/
+
+
+
+
+
+def instaScrap():
+	url = 'https://www.instagram.com/explore/tags/test/'
+	driver = webdriver.Firefox()
+	driver.get(url)
+
+	soup = BeautifulSoup(driver.page_source,features="html.parser")
+
+
+	num = 0
+
+	for x in soup.findAll('img', {'class':'FFVAD'}):
+		url = x['src']
+		# print(url)
+
+		page = requests.get(url)
+		f_ext = os.path.splitext(url)[-1]
+		# f_name = "img{}".format(f_ext)
+		# f_name = str(num).format(f_ext)
+		f_name = os.path.basename(url)
+
+		print(str(num) + " -> " + f_name)
+
+		num += 1
+
+		with open('images/'+f_name, 'wb') as f:
+			f.write(page.content)
+
+	driver.close()
+
+	Timer(30.0, instaScrap).start()
+
+Timer(0.0, instaScrap).start()
